83cbd92a593dc8443f69ebb84bf54f0d18bfc31b
Janamjay <jaydevvv23@gmail.com>
2025-12-26 12:24:09 +0530

    commented

--- src/layouts/GoogleCallback.jsx
+++ src/layouts/GoogleCallback.jsx
@@ -7,25 +7,25 @@ const GoogleCallback = () => {
   const [searchParams] = useSearchParams();
   const hasNavigated = useRef(false);
 
-  useEffect(() => {
-    if (hasNavigated.current) return;
+  // useEffect(() => {
+  //   if (hasNavigated.current) return;
 
-    const idToken = searchParams.get("id_token");
+  //   const idToken = searchParams.get("id_token");
 
-    if (idToken) {
-      localStorage.setItem("jwt", idToken);
+  //   if (idToken) {
+  //     localStorage.setItem("jwt", idToken);
 
-      hasNavigated.current = true;
-      navigate("/", { replace: true });
-    } else {
-      const modalEl = document.getElementById("sign_up_popup2");
+  //     hasNavigated.current = true;
+  //     navigate("/", { replace: true });
+  //   } else {
+  //     const modalEl = document.getElementById("sign_up_popup2");
 
-      if (modalEl) {
-        const signInModal = new window.bootstrap.Modal(modalEl);
-        signInModal.show();
-      }
-    }
-  }, [navigate, searchParams]);
+  //     if (modalEl) {
+  //       const signInModal = new window.bootstrap.Modal(modalEl);
+  //       signInModal.show();
+  //     }
+  //   }
+  // }, [navigate, searchParams]);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
